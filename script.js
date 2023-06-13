2e047f1c63c26b1b555aac6659405ef37336343d
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-06-13 21:15:00 +0600

    pdf drive, gutenberg genesis

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 function searchGoogle() {
   var query = document.getElementById("query").value;
   var concatenatedQuery = query + " Doctype.pdf";
-  var searchUrl =
+  var googleUrl =
     "https://www.google.com/search?q=" + encodeURIComponent(concatenatedQuery);
-
-  window.open(searchUrl, "_blank");
+    
+  console.log(googleUrl);
+  window.open(googleUrl, "_blank");
 }
+
+function searchPDFDrive() {
+  var query = document.getElementById("query").value;
+  var pdfDriveUrl =
+    "https://www.pdfdrive.com/search?q=" +
+    encodeURIComponent(query);
+  console.log(pdfDriveUrl);
+  window.open(pdfDriveUrl, "_blank");
+}
+
+function searchGutenberg() {
+  var query = document.getElementById("query").value;
+  var GutenbergUrl =
+    "https://www.gutenberg.org/ebooks/search/?query=" +
+    encodeURIComponent(query);
+  console.log(GutenbergUrl);
+  window.open(GutenbergUrl, "_blank");
+}
+
+function searchGenesis() {
+  var query = document.getElementById("query").value;
+  var GenesisUrl =
+    "https://libgen.is/search.php?req=" + encodeURIComponent(query);
+  console.log(GenesisUrl);
+  window.open(GenesisUrl, "_blank");
+}
+
