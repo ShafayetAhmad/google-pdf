99e90f12f65fa30699ec2a8a8aa61a8ac3276854
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-06-13 13:08:50 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function searchGoogle() {
+  var query = document.getElementById("query").value;
+  var concatenatedQuery = query + " Doctype.pdf";
+  var searchUrl =
+    "https://www.google.com/search?q=" + encodeURIComponent(concatenatedQuery);
+
+  window.open(searchUrl, "_blank");
+}
